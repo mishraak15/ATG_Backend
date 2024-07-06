9683f0283edcb3aa14bb0d06c37bb049d095cbcc
mishraak15 <133697079+mishraak15@users.noreply.github.com>
2024-07-06 16:24:17 +0530

    Update homeRoute.js

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -7,25 +7,30 @@ const {
   login,
   forgetPassword,
   resetPassword,
+  protect,
 } = require("../controllers/authController");
 const { fetchJobs } = require("../controllers/postController");
 
 router.get("/", ((req,res,next)=>{
-     res.json({msg:"OK"});
+  res.json({msg:"OK"});
 }));
 
 router.post("/signup", signup);
+
 
 router.get("/logout", logout);
 
 router.post("/login", login);
 
-router.get("/fetchJobs/:category", fetchJobs);
 
 router.post("/forgetpassword", forgetPassword);
 
 router.patch("/resetpassword/:token", resetPassword);
 
 router.get("/verifyemail/:Code", verifyEmailHandler);
+
+router.use(protect);
+
+router.get("/fetchJobs/:category", fetchJobs);
 
 module.exports = router;
